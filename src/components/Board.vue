9c1659eceb6fc35abb7036ad19b3864ac344dcfd
RyanGormican <ryan.gormican@gmail.com>
2023-11-21 01:37:45 -0600

    a
    a

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
-    <canvas ref="canvas" @mousedown="startDrawing" @mousemove="draw" @mouseup="stopDrawing" @mouseleave="stopDrawing"></canvas>
-    <button class="clear-button" @click="clearCanvas">
-      <Icon icon="ph:trash" height='60' width='60' />
-    </button>
+    <div class="canvas-container">
+      <div class="canvas-wrapper">
+        <canvas ref="canvas" @mousedown="startDrawing" @mousemove="draw" @mouseup="stopDrawing" @mouseleave="stopDrawing"></canvas>
+      </div>
+      <div class="button-container">
+        <button class="clear-button" @click="clearCanvas">
+          <Icon icon="ph:trash" width="60" />
+        </button>
+      </div>
+    </div>
     <div class="palette">
       <div v-for="(color, index) in paletteColors" :key="index" @click="selectColor(color)" :style="{ backgroundColor: color }"></div>
     </div>
@@ -11,12 +17,17 @@
 </template>
 
 <script>
+  import { Icon } from '@iconify/vue';
+
   export default {
+  components: {
+  Icon,
+  },
   data() {
   return {
   drawing: false,
   selectedColor: 'red',
-  paletteColors: ['#ff0000','#ffae00','#ffff5c','#00ff00', '#0000ff','#3f0fff'],
+  paletteColors: ['#ff0000', '#ffae00', '#ffff5c', '#00ff00', '#0000ff', '#3f0fff'],
   };
   },
   methods: {
@@ -28,13 +39,13 @@
   this.drawing = false;
   },
   clearCanvas() {
-  const canvas=this.$refs.canvas;
+  const canvas = this.$refs.canvas;
   const context = canvas.getContext('2d');
 
-  context.clearRect(0,0,canvas.width,canvas.height);
+  context.clearRect(0, 0, canvas.width, canvas.height);
   },
-  selectColor(color){
-  this.selectedColor=color;
+  selectColor(color) {
+  this.selectedColor = color;
   },
   draw(event) {
   if (!this.drawing) return;
@@ -59,7 +70,6 @@
   canvas.height = 0.6 * window.innerHeight;
 
   // Center the canvas
-  canvas.style.margin = 'auto';
   canvas.style.display = 'block';
 
   // Set initial color
@@ -69,25 +79,44 @@
 </script>
 
 <style scoped="">
-  canvas {
-  border: 3px solid #000;
+  .canvas-container {
+  display: flex;
+  justify-content: center;
   }
+
+  .canvas-wrapper {
+  position: relative;
+  }
+
+  .button-container {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  background-color: transparent;
+  }
+
   .clear-button {
-  background: none;
   border: none;
   cursor: pointer;
   outline: none;
+  background-color: transparent;
+  }
+
+  canvas {
+  border: 3px solid #000;
   }
 
   .palette {
   display: flex;
-  margin:auto;
-  justify-content:center;
+  margin: auto;
+  justify-content: center;
   }
+
   .palette div {
   width: 4vw;
-  height:4vh;
-  cursor:pointer;
+  height: 4vh;
+  cursor: pointer;
   border: 1px solid #000;
   }
 </style>
+
